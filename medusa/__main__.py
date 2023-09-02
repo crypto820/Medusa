3f62922f0caf530df0343886630f010856277992
philoinovsky <1129410550@qq.com>
2023-09-02 20:10:24 +0800

    update

--- medusa/__main__.py
+++ medusa/__main__.py
@@ -1,18 +1,46 @@
 import argparse
 import logging
+import base64
+from functools import reduce
+from typing import List
+
+import requests
+from urllib.parse import urlparse, ParseResult
+
+from medusa.config import config, template
+from medusa.subconverter import SubConverter
 
 
 def setup_logger():
-    logging.basicConfig()
+    logging.basicConfig(level=logging.INFO,
+                        format="[%(asctime)s] [%(levelname)s] - %(message)s - [%(filename)s:%(lineno)d]",
+                        datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def fetch_config(url: str) -> List[ParseResult]:
+    logging.info(f"Handling subscription {url}")
+    b64 = requests.get(url)
+    hosts = base64.b64decode(b64.content).decode().splitlines(keepends=False)
+    print(hosts[0])
+    return [urlparse(host) for host in hosts if len(host)]
 
 
 def main():
+    setup_logger()
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", type=str, required=True)
     parser.add_argument("-o", "--output", type=str, required=True)
-    parser.add_argument("--frontend", type=str, required=False, default="clash")
     parser.add_argument("--backend", type=str, required=False, default="glider")
     args = parser.parse_args()
+    pr_list = list()
+    for url in config()["subscriptions"]:
+        results = fetch_config(url)
+        pr_list.append(SubConverter.convert(args.backend, results))
+
+    result = list(map(lambda e: f"{e}\n", reduce(list.__add__, pr_list)))
+    with open(args.output, "w") as f:
+        content = template(args.backend)
+        f.writelines(content)
+        f.writelines(result)
     return
 
 
---
+++ medusa/subconverter.py
@@ -0,0 +1,37 @@
+import base64
+import logging
+from typing import Literal, List
+from urllib.parse import ParseResult, unquote
+
+
+def b64decode_urlsafe(s: str) -> str:
+    missing_padding = len(s) % 4
+    if missing_padding != 0:
+        s += '=' * (4 - missing_padding)
+    return base64.urlsafe_b64decode(s).decode()
+
+
+class SubConverter:
+    @staticmethod
+    def convert(backend: str, parse_results: List[ParseResult]):
+        match backend:
+            case "glider":
+                return SubConverter._to_glider(parse_results)
+            case _:
+                raise NotImplementedError
+
+    @staticmethod
+    def _to_glider(parse_results: List[ParseResult]):
+        def handle_ss(ss: ParseResult):
+            return ''.join(("forward=", ss.scheme, "://", b64decode_urlsafe(ss.username), "@", ss.hostname, ":", str(ss.port), "#", str(ss.fragment)))
+
+        def handle_trojan(tj: ParseResult):
+            return ''.join(("forward=", tj.scheme, "://", tj.username, "@", tj.hostname, ":", str(tj.port), "?", tj.query, "#", str(tj.fragment)))
+
+        res = list()
+        for r in parse_results:
+            logging.info(f"Handling {r}")
+            pr = eval(f"handle_{r.scheme}")(r)
+            logging.info(f"Constructed '{pr}'")
+            res.append(pr)
+        return res
